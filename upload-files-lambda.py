19d58f9519a4503502fba0eea469ca3808b8bda6
sunlee <sunlee128@gmail.com>
2020-06-08 00:19:08 +1000

    updated upload_files_lambda

--- upload-files-lambda.py
+++ upload-files-lambda.py
@@ -1,14 +1,15 @@
 import boto3
 from botocore.client import Config
-import StringIO
+import io
 import zipfile
+import mimetypes
 
-s3 = boto3.resource('s3')
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
 portfolio_bucket = s3.Bucket('portfolio.sunlee.info')
 build_bucket = s3.Bucket('portfoliobuild.sunlee.info')
 
-portfolio_zip = StringIO.StringIO()
+portfolio_zip = io.BytesIO()
 build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
 with zipfile.ZipFile(portfolio_zip) as myzip:
@@ -17,5 +18,3 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
         portfolio_bucket.upload_fileobj(
             obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    return 'Hello from Lambda'
